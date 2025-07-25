916fddbce436abf0a46c3ae050cfb410ba756ec6
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-25 12:05:02 +0200

    connected to db

--- src/main/java/andream/gestioneprenotazioni/GestioneprenotazioniApplication.java
+++ src/main/java/andream/gestioneprenotazioni/GestioneprenotazioniApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestioneprenotazioniApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestioneprenotazioniApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneprenotazioniApplication.class, args);
+
+
+    }
 
 }
